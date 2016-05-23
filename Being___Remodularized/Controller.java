655528882810e880a711b58ae3f37c2364417234
VanDeniz <zhibin@cs.uoregon.edu>
2016-05-23 07:21:30 -0700

    Create Controller.java

---
+++ Being___Remodularized/Controller.java
@@ -0,0 +1,38 @@
+import java.applet.Applet;
+import java.awt.Frame;
+
+public class Controller extends Applet {
+	
+	Thread animation;
+	Model model = new Model();
+	View view = new View();
+	
+	public void init() {
+		setSize(900, 600);
+		setFocusable(true);
+		
+		Frame frame = (Frame) this.getParent().getParent();
+		frame.setTitle("Factory Bird");	
+		
+		animation = new Thread(view);
+		animation.start();
+		
+		// preparing parameters for view to display
+		view.setDelay(17); // view.delay is always 17
+		view.setModel(model);
+		view.setController(this);
+	}
+	
+	public void start() {
+		view.setAlive(true);
+		view.setRunning(true);
+	}
+	
+	public void stop() {
+		view.setRunning(false);
+	}
+	
+	public void destroy() {
+		view.setAlive(false);
+	}
+}

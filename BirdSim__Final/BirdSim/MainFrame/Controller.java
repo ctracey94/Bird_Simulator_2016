5de36564d23d0a8ea2c515032edc0104b5a4f1e5
VanDeniz <zhibin@cs.uoregon.edu>
2016-05-29 00:46:56 -0700

    Update Controller.java

--- BirdSim__Final/BirdSim/MainFrame/Controller.java
+++ BirdSim__Final/BirdSim/MainFrame/Controller.java
@@ -17,31 +17,34 @@ import javax.swing.SwingUtilities;
 
 @SuppressWarnings("serial")
 public class Controller extends JFrame implements KeyListener {
+	
+	Model model;
+	View view;
+	Thread animation;
+	
 	public Controller() {
 		init();
 	}
 	
-	Model model = new Model();
-	View view = new View();
-	Thread animation;
-	
+	// set up JFrame
 	public void init() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    
 		setVisible(true);
-		
 		setSize(900, 600);
 		setFocusable(true);
+		setTitle("Bird Sim");	
 		addKeyListener(this);
-		
+	}
+	
+	public void start() {
+		model = new Model();
+		view = new View();
+		Model.state = Model.GameState.Title;
 		setLayout(new BorderLayout());
 		this.add(BorderLayout.CENTER, view);
-		setTitle("Bird Sim");	
-		
-		// preparing parameters for view to display
 		view.setModel(model);
-		view.setDelay(17); // view.delay is always 17
-		try {view.loadMap("Animation/resources/map.txt");} 
+		
+		try {view.loadMap("BirdSim/Animation/resources/map.txt");} 
 		catch (IOException e) {e.printStackTrace();}
 		view.loadAssets();
 		
@@ -49,42 +52,39 @@ public class Controller extends JFrame implements KeyListener {
 		animation.start();
 	}
 	
-	
-	
 	@SuppressWarnings("static-access")
 	public void keyPressed(KeyEvent e) {
 		
 		switch (e.getKeyCode()) {
 			
 			case KeyEvent.VK_UP: case KeyEvent.VK_W:
-				Model.getBird().jump();
-				System.out.println("Jumped");
+				if (Model.getBird().isAlive())
+					Model.getBird().jump();
 				break;
 				
 			case KeyEvent.VK_DOWN: case KeyEvent.VK_S:
-				Model.getBird().diveBomb();
-				System.out.println("Diving");
+				if (Model.getBird().isAlive())
+					Model.getBird().diveBomb();
 				break;
 			
 			case KeyEvent.VK_LEFT: case KeyEvent.VK_A:
-				Model.getBird().moveLeft();
-				System.out.println("Moving left");
+				if (Model.getBird().isAlive())
+					Model.getBird().moveLeft();
 				break;
 			
 			case KeyEvent.VK_RIGHT: case KeyEvent.VK_D:
-				Model.getBird().moveRight();
-				System.out.println("Moving right");
+				if (Model.getBird().isAlive())
+					Model.getBird().moveRight();
 				break;
 			
 			case KeyEvent.VK_SPACE: case KeyEvent.VK_K:
-				Model.getBird().glide();
-				System.out.println("Gliding");
+				if (Model.getBird().isAlive())
+					Model.getBird().glide();
 				break;
 				
 			case KeyEvent.VK_ENTER:
 				if (Model.state == Model.state.Title) 
-					view.setPlaying(true);
-				break;
+					Model.state = Model.GameState.Running;
 		}
 		
 	}
@@ -123,7 +123,8 @@ public class Controller extends JFrame implements KeyListener {
 		SwingUtilities.invokeLater(new Runnable(){
 			@Override
 			public void run() {
-				new Controller();
+				JFrame frame = new Controller();
+				((Controller) frame).start();
 			}
 		});
 	}
